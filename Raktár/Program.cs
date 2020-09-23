d5d7a34f19c784095c2cbbec88a1106879288f4f
mihalydaniel22 <rikkiardo3@gmail.com>
2020-09-23 14:46:45 +0200

    Termék osztály létrehozása

--- Raktár/Program.cs
+++ Raktár/Program.cs
@@ -10,6 +10,8 @@ namespace Raktár
     {
         static void Main(string[] args)
         {
+
+            Console.ReadKey();
         }
     }
 }
---
+++ Raktár/Termek.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raktár
+{
+    class Termek
+    {
+        private string kod;
+
+        public string Kod
+        {
+            get { return kod; }
+            set { kod = value; }
+        }
+        private string nev;
+
+        public string Nev
+        {
+            get { return nev; }
+            set { nev = value; }
+        }
+        private int ar;
+
+        public int Ar
+        {
+            get { return ar; }
+            set { ar = value; }
+        }
+        private int db;
+
+        public int DB
+        {
+            get { return db; }
+            set { db = value; }
+        }
+
+        public Termek(string kod, string nev, int ar, int db)
+        {
+            this.kod = kod;
+            this.nev = nev;
+            this.ar = ar;
+            this.db = db;
+        }
+    }
+}
