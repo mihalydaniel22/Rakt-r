de1d9e21705705efdafbdd8456644187eafa03c4
mihalydaniel22 <rikkiardo3@gmail.com>
2020-09-24 08:29:41 +0200

    Raktár metódus elkészítése

--- Raktár/Program.cs
+++ Raktár/Program.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Raktár
 {
     class Program
     {
+        static List<Termek> termekek = new List<Termek>();
+        static void BeolvasRaktar()
+        {
+            StreamReader raktar = new StreamReader("raktar.csv");
+            while (!raktar.EndOfStream)
+            {
+                string[] sor = raktar.ReadLine().Split(';');
+                termekek.Add(new Termek(sor[0], sor[1], int.Parse(sor[2]), int.Parse(sor[3]))); 
+            }
+            raktar.Close();
+        }
         static void Main(string[] args)
         {
-
+            BeolvasRaktar();
             Console.ReadKey();
         }
     }
